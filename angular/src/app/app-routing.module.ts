827633ff268a6948b2ff7a4a4a15c4a4e55ded36
mborroni <miliborroni@gmail.com>
2019-01-04 14:17:18 -0300

    login/register routes

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { TasksBoardComponent } from './layout/tasks-board/tasks-board.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
+  // {
+  //   path: '', redirectTo: 'projects', pathMatch: 'full'
+  // },
   {
-    path: '', redirectTo: 'projects', pathMatch: 'full'
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
   },
   {
     path: 'projects', component: HomePageComponent,
